0a63d138192a2d613ea8edd2a26f8e75ee057134
beaniejoy <lhb1025@gmail.com>
2021-09-13 01:06:59 +0900

    first commit to develop branch

---
+++ src/main/java/io/beaniejoy/triplemileage/TripleMileageServiceApplication.java
@@ -0,0 +1,13 @@
+package io.beaniejoy.triplemileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleMileageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripleMileageServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/beaniejoy/triplemileage/TripleMileageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.beaniejoy.triplemileage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleMileageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
